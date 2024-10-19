f2b196aabf8f9d956a04c760368b94d14234246d
Ruan Lucas <rulucasilva13@gmail.com>
2024-10-19 15:34:31 -0300

    Solução muito ruim

---
+++ src/application/program.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+public class program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner SC = new Scanner(System.in);
+	 
+		
+		System.out.println("Informe os dados da conta");
+		System.out.print("Numero:");
+		int number = SC.nextInt();
+		System.out.print("Titular:");
+		SC.nextLine();
+		String holder = SC.nextLine();
+		System.out.print("Saldo inicial:");
+		double balance = SC.nextDouble();
+		System.out.println("Limite de saque:");
+		double withdrawLimit = SC.nextDouble();
+		
+		Account account = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Informe a quantia para sacar:");
+		double amount = SC.nextDouble();
+		
+		if(amount > account.getWithdrawLimit()) {
+			System.out.println("Erro de saque: a quantia excede o limite de saque");
+		}
+		else if(amount > account.getBalance()){
+			System.out.println("Erro de saque: Saldo insuficiente");
+		}
+		else {
+			account.withdraw(amount);
+			System.out.printf("Novo saldo:" + String.format("%.2f", account.getBalance()));
+		}
+			
+		SC.close();
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module exercicioExceçoes {
+}
